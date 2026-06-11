f87071f823a18824ea902610d6fb86058291b017
anniesit <apysit@gmail.com>
2026-06-11 14:21:21 +0800

    Fix input styling

--- bundles/all.js
+++ bundles/all.js
@@ -4,7 +4,7 @@
  * To make changes, edit the source files in /global or /components,
  * then run: bash build.sh
  *
- * Built: 2026-06-11 14:11:16
+ * Built: 2026-06-11 14:21:05
  * ============================================================ */
 
 
